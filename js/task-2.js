7a8eaa762308a8151836a711a43697f2dbf1acca
Soyakam <108721019+Soyakam@users.noreply.github.com>
2024-01-08 10:59:57 +0100

    Update task-2.js

--- js/task-2.js
+++ js/task-2.js
@@ -1,6 +1,9 @@
 function getShippingMessage(country, price, deliveryFee) {
     let totalPrice = price + deliveryFee;
-    return "Shipping to" + country - "will cost" + totalPrice + "credits";
+    return `Shipping to ${country} will cost ${totalPrice} credits` ;
 }
+
+
+
 
 
